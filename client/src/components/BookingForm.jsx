69b4d44ff0680e4344cf46e587a9904c6446281c
Thilaksan8 <thilakshanlat@gmail.com>
2026-07-28 04:49:54 +0530

    Implement controlled input for booking form

--- client/src/components/BookingForm.jsx
+++ client/src/components/BookingForm.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 function BookingForm() {
+
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
+    const [timeSlot, setTimeSlot] = useState("");
+    const [category, setCategory] = useState("");
+    const [note, setNote] = useState("");
+
     return (
         <div>
+
             <h2>Book a Time Slot</h2>
+
+            <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <p>{name}</p>
+
         </div>
     );
+
 }
 
 export default BookingForm;
