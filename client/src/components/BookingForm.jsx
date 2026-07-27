965baee03740ca1fe7502047eb61787d4d28aa2f
Thilaksan8 <thilakshanlat@gmail.com>
2026-07-28 05:24:15 +0530

    Build booking form with controlledd inputs

--- client/src/components/BookingForm.jsx
+++ client/src/components/BookingForm.jsx
@@ -8,6 +8,14 @@ function BookingForm() {
     const [category, setCategory] = useState("");
     const [note, setNote] = useState("");
 
+    const handleSubmit = () => {
+        console.log(name);
+        console.log(date);
+        console.log(timeSlot);
+        console.log(category);
+        console.log(note);
+    }
+
     return (
         <div>
 
@@ -19,8 +27,45 @@ function BookingForm() {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
-            <p>{name}</p>
-
+            <br/>
+            <input
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+            />  
+            <br/>
+            <select
+                value={timeSlot}
+                onChange={(e) => setTimeSlot(e.target.value)}
+            >
+                <option value="">Select a time slot</option>
+                <option >9:00 - 9:30 AM</option>
+                <option >9:30 - 10:00 AM</option>
+                <option >10:00 - 10:30 AM</option>
+                <option >11:00 - 11:30 AM</option>
+            </select>
+            <br/>
+            <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}   
+            >
+                <option value="">Select a category</option>
+                <option>Meeting</option>
+                <option>Interview</option>
+                <option>Discussion</option>
+                <option>Consultation</option>
+            </select>
+            <br/>
+            <textarea
+                rows="4"
+                placeholder="Enter any notes"
+                value={note}
+                onChange={(e) => setNote(e.target.value)}
+            ></textarea>
+            <br/>
+            <button onClick={handleSubmit}>Book Slot</button>
+            
+                
         </div>
     );
 
